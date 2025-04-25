7ec6e06b2b60a0e063c0f9d408a5b44dbcd095fb
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-04-25 11:04:58 +0530

    Time: 0 ms (100%), Space: 9 MB (70.59%) - LeetHub

--- 3046-minimum-operations-to-make-a-special-number/3046-minimum-operations-to-make-a-special-number.cpp
+++ 3046-minimum-operations-to-make-a-special-number/3046-minimum-operations-to-make-a-special-number.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     int minimumOperations(string s) {
-        int n = s.size();
-        int mini = n;
-
-        if(n == 1 && s[0] == '0') return 0;
-        if(n == 1) return 1;
-
-        for(int j = n - 1; j >= 0; j--) {
-            if(s[j] == '0') {
-                for(int i = j - 1; i >= 0; i--) {
-                    if(s[i] == '0' || s[i] == '5') {
-                        mini = min(mini, n - i - 2);
-                        break;
-                    }
-                }
-            }
-            if(s[j] == '5') {
-                for(int i = j - 1; i >= 0; i--) {
-                    if(s[i] == '2' || s[i] == '7') {
-                        mini = min(mini, n - i - 2);
-                        break;
-                    }
-                }
-            }
+        int n=s.size();
+        int mini=n;
+        if(n==1 && s[0]=='0'){
+            return 0;
         }
-        for(int i = 0; i < n; i++) {
-            if(s[i] == '0') {
-                mini = min(mini, n - 1);
+        if(n==1)return 1;
+
+        int j=n-1;
+        while(j>=0){
+            if(s[j]=='0'){
+                int i=j-1;
+                while(i>=0 && s[i]!='0' && s[i]!='5'){
+                    i--;
+                }
+                if(i>=0)mini=min(mini,n-i-2);
+            }
+            if(s[j]=='5'){
+                int i=j-1;
+                while(i>=0 && s[i]!='2' && s[i]!='7'){
+                    i--;
+                }
+                if(i>=0)mini=min(mini,n-i-2);
+            }
+            j--;
+        }
+
+        // special case to make number "0"
+        int f=0,i=0;
+        while(i<n){
+            if(s[i]=='0'){
+                f=1;
                 break;
             }
+            i++;
         }
+        if(f)mini=min(mini,n-1);
 
         return mini;
     }
