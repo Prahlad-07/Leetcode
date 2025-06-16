c85ca6deb3e4a2c9770f2a0479c3bb1e803ddfe9
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-06-17 03:29:46 +0530

    Time: 1964 ms (5.01%), Space: 437.8 MB (12.28%) - LeetHub

---
+++ 3885-count-special-triplets/3885-count-special-triplets.cpp
@@ -0,0 +1,28 @@
+class Solution {
+const int mod = 1e9 + 7;
+public:
+    int specialTriplets(vector<int>& nums) {
+        int n = nums.size();
+        map<int, int> total, left;
+
+        for (int x : nums) total[x]++;
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            int x = nums[i];
+            total[x]--; 
+
+            int y = 2 * x;
+            int before = left[y];
+            int after = total[y];
+
+            if (before > 0 && after > 0) {
+                ans = (ans + 1LL * before * after % mod) % mod;
+            }
+
+            left[x]++;
+        }
+
+        return ans;
+    }
+};
