d01e74ad819aa2e8b5ab60952b5bd24bbe34933b
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-07-07 02:18:12 +0530

    Time: 654 ms (11.47%), Space: 194.4 MB (7.78%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -1,26 +1,41 @@
 class FindSumPairs {
-    vector<int>nums1, nums2;
-    map<int, int> freq2;  // Maintain frequency map for nums2
-
+    vector<int>nums1,nums2;
+    map<int,int>mp;
 public:
     FindSumPairs(vector<int>& arr, vector<int>& brr) {
         nums1 = arr;
         nums2 = brr;
-        for (int x : nums2) freq2[x]++;
+        for(auto & val : nums2)mp[val]++;
     }
     
     void add(int index, int val) {
-        freq2[nums2[index]]--;  // Remove old value count
+        mp[nums2[index]]--;
         nums2[index] += val;
-        freq2[nums2[index]]++;  // Add new value count
+        mp[nums2[index]]++;
+        
     }
     
     int count(int tot) {
         int cnt = 0;
-        for (int a : nums1) {
-            int need = tot - a;
-            cnt += freq2[need];
+        // for(int i = 0;i < nums1.size(); i++){
+        //     for(int j = 0; j < nums2.size(); j++){
+        //         long long sum = nums1[i] + nums2[j];
+        //         if(sum ==tot)cnt++;
+
+        //     }
+        // }
+        for (int x : nums1) {
+            int need = tot - x;
+            cnt += mp[need];
         }
         return cnt;
+        
     }
 };
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
