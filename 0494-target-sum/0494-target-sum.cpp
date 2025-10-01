fc94371b46f9f1eb054b69379e835a610d9f22f1
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-10-01 14:05:02 +0530

    Time: 7 ms (69.75%), Space: 12.6 MB (66.84%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,18 @@
 class Solution {
+    int dp[101][2001];
     int solve(vector<int>&nums, int idx, int n, int target, int sum){
         if(idx >= n){
             return sum == target ? 1 : 0;
         }
-        
+        if(dp[idx][sum + 1000] != -1)return dp[idx][sum + 1000];
         int plus = solve(nums, idx + 1, n, target, sum + nums[idx]);
         int minus = solve(nums, idx + 1, n, target, sum - nums[idx]);
-        return plus + minus;
+        return dp[idx][sum + 1000] = plus + minus;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         return solve(nums, 0, n, target, 0);
     }
 };
