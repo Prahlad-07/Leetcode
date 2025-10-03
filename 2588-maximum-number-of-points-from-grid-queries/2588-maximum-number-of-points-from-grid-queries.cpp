d65200b41c1e56c1c4d0756e155dfe699bee9269
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-10-03 16:21:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -19,46 +19,46 @@ private:
 
 public:
     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
-        // vector<int> ans;
-        // for(auto &val : queries) {
-        //     vector<vector<bool>> vist(grid.size(), vector<bool>(grid[0].size(), false));
-        //     int cnt = 0;
-        //     int points = DFS(grid, 0, 0, val, cnt, vist);  
-        //     ans.push_back(points);
-        // }
-        // return ans;
-
-       vector<int> ans(queries.size(), 0);  
-        vector<pair<int, int>> arr;
-        for(int i = 0; i < queries.size(); i++)
-            arr.push_back({queries[i], i});
-        sort(arr.begin(), arr.end());
-
-        int cnt = 0;
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
-        pq.push({grid[0][0], {0, 0}});
-        visited[0][0] = true;
-
-        for(auto& [val, idx] : arr) {
-            while(!pq.empty() && pq.top().first < val) {
-                auto [value, position] = pq.top();
-                pq.pop();
-                cnt++;
-                int i = position.first;
-                int j = position.second;
-
-                for(auto& [dx, dy] : dir) {
-                    int ni = i + dx;
-                    int nj = j + dy;
-                    if(ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size() && !visited[ni][nj]) {
-                        visited[ni][nj] = true;
-                        pq.push({grid[ni][nj], {ni, nj}});
-                    }
-                }
-            }
-            ans[idx] = cnt;
+        vector<int> ans;
+        for(auto &val : queries) {
+            vector<vector<bool>> vist(grid.size(), vector<bool>(grid[0].size(), false));
+            int cnt = 0;
+            int points = DFS(grid, 0, 0, val, cnt, vist);  
+            ans.push_back(points);
         }
         return ans;
+
+    //    vector<int> ans(queries.size(), 0);  
+    //     vector<pair<int, int>> arr;
+    //     for(int i = 0; i < queries.size(); i++)
+    //         arr.push_back({queries[i], i});
+    //     sort(arr.begin(), arr.end());
+
+    //     int cnt = 0;
+    //     vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+    //     priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    //     pq.push({grid[0][0], {0, 0}});
+    //     visited[0][0] = true;
+
+    //     for(auto& [val, idx] : arr) {
+    //         while(!pq.empty() && pq.top().first < val) {
+    //             auto [value, position] = pq.top();
+    //             pq.pop();
+    //             cnt++;
+    //             int i = position.first;
+    //             int j = position.second;
+
+    //             for(auto& [dx, dy] : dir) {
+    //                 int ni = i + dx;
+    //                 int nj = j + dy;
+    //                 if(ni >= 0 && ni < grid.size() && nj >= 0 && nj < grid[0].size() && !visited[ni][nj]) {
+    //                     visited[ni][nj] = true;
+    //                     pq.push({grid[ni][nj], {ni, nj}});
+    //                 }
+    //             }
+    //         }
+    //         ans[idx] = cnt;
+    //     }
+    //     return ans;
     }
 };
