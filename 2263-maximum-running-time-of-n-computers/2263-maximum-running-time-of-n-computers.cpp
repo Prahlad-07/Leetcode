3ee5fe1df8f2838669034fb7f9e0d209761bdfc1
Prahlad yadav <128887097+Prahlad-07@users.noreply.github.com>
2025-10-25 15:56:32 +0530

    Time: 47 ms (11.99%), Space: 59.8 MB (27.79%) - LeetHub

---
+++ 2263-maximum-running-time-of-n-computers/2263-maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool isValid(vector<int>& batteries, int n, long long mid){
+        int cnt = 0;
+        long long rem = 0;
+        for(auto &val : batteries){
+            if(val + rem >= mid){
+                rem = (val + rem) - mid;
+                cnt++;
+            }
+            else{
+                rem += val;
+            }
+            if(cnt >= n) return true;
+        }
+        return cnt >= n;
+    }
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long sum = 0;
+        for(auto &b : batteries) sum += b;
+        sort(batteries.begin(), batteries.end());
+        long long low = 0, high = sum / n;
+        long long ans = 0;
+        while(low <= high){
+            long long mid = (low + high)/2;
+            if(isValid(batteries, n, mid)){
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
